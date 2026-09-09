cca83610bb43878822ecbb96c42c7b29f466412f
itsrahul9711 <rahulkaler9711@gmail.com>
2026-09-09 17:52:14 +0530

    updated

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
         version = "4.70.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "jagga"
+    storage_account_name = "dondu_storage"
+    container_name = "tfstate"
+    key = "Rahul.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -18,7 +24,20 @@ resource "azurerm_resource_group" "dondu" {
   location = "eastus"
 }
 
-resource "azurerm_resource_group" "dondu1" {
-  name = "jagga1"
-  location = "eastus"
+resource "azurerm_storage_account" "jatt" {
+    depends_on = [ azurerm_resource_group.dondu ]
+  name = "dondu_storage"
+  location = "east us"
+  resource_group_name = "jagga"
+  account_replication_type = "LRS"
+  account_tier = "Standard"
+
 }
+resource "azurerm_storage_container" "container_name" {
+    depends_on = [ azurerm_storage_account.jatt ]
+    name = "tfstate"
+    storage_account_id = azurerm_storage_account.tfstate.id
+    container_access_type = "private"
+  
+}
+
