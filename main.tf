04596de228535ed1063314f5e5b7b13ea41efe36
itsrahul9711 <rahulkaler9711@gmail.com>
2026-09-09 20:56:35 +0530

    rg added

--- main.tf
+++ main.tf
@@ -1,36 +1,25 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "4.70.0"
+      source  = "hashicorp/azurerm"
+      version = "4.70.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name = "jagga"
-    storage_account_name = "dondustorage"
-    container_name = "bopu"
-    key = "rahul.tftsate"
-  }
-  }
+}
 
 
 provider "azurerm" {
   features {
-    
+
   }
 }
 
-resource "azurerm_resource_group" "dondu" {
-  name = "jagga"
+resource "azurerm_resource_group" "bo" {
+  name     = "hondu"
   location = "eastus"
 }
 
-resource "azurerm_storage_account" "jatt" {
-    depends_on = [ azurerm_resource_group.dondu ]
-  name = "dondustorage"
-  location = "east us"
-  resource_group_name = "jagga"
-  account_replication_type = "LRS"
-  account_tier = "Standard"
-
+resource "azurerm_resource_group" "so" {
+  name     = "sondu"
+  location = "eastus"
 }
